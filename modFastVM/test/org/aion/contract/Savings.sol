9a3ce4263a128dd051d56f089728679d1c5244d4
aionick <nick@nuco.io>
2018-09-13 15:36:58 -0400

    added report

--- modFastVM/test/org/aion/contract/Savings.sol
+++ modFastVM/test/org/aion/contract/Savings.sol
@@ -88,7 +88,7 @@ contract Savings is Owned {
 	uint public t0special;
 
 	uint constant public intervalSecs = 3 seconds;
-	uint constant public precision = 10 ** 7;
+	uint constant public precision = 10 ** 2;
 
 
 	/**
